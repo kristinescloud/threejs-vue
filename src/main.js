22529a4090e33b759da310beccce9b68eb3d777f
kristinescloud <kristine.uchendu@gmail.com>
2025-06-03 10:28:31 -0400

    cleaning up

--- src/main.js
+++ src/main.js
@@ -5,10 +5,7 @@ import App from './App.vue'
 import Controller from './components/Controller.vue'
 import Viewer from './components/Viewer.vue'
 import MyApp from './MyApp.vue'
-import router from './router'
 
 const app = createApp(MyApp)
-
-app.use(router)
 
 app.mount('#app')
